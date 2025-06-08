e4ac16a91d8753307b4dbee27f36b65674141014
Krakus37 <szkrakus@op.pl>
2025-06-08 10:01:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>OrlikConnect</h1>
+      <p>Twoja aplikacja do umawiania meczów</p>
+    </main>
+  );
+}
